1f2f282a61d088b9d7397f9a93de579119997b68
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-27 17:17:20 +0200

    Add the project

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# random-quote-machine
+# Project Name
+
+## Description
+
+This project was created using Vite, React, JavaScript, CSS, HTML, and Node.js. It showcases a [brief description of your project's main functionality or purpose].
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Technologies Used](#technologies-used)
+- [License](#license)
+
+## Installation
+
+1. Clone this repository to your local machine using:
+
+   ```sh
+   git clone https://github.com/NkululekoCyrilCele/random-quote-machine.git
+   ```
+
+2. Navigate to the project directory:
+
+```sh
+cd random-quote-machine
+```
+
+3. Install dependencies:
+
+```sh
+npm install
+```
+
+## Usage
+
+1. Run the development server:
+
+```sh
+npm run dev
+```
+
+Open your web browser and visit http://localhost:<port> to see the project in action.
+
+## Technologies Used
+
+- Vite
+- React
+- JavaScript
+- CSS
+- HTML
+- Node.js
---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+"use strict";
+
+import { useState, useEffect } from "react";
+import { FaTwitter, FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import "./App.css";
+
+function App() {
+  const [quote, setQuote] = useState({ quote: "", author: "" });
+  const [quoteColor, setQuoteColor] = useState(getRandomColor());
+
+  const changeQuote = async () => {
+    const randomQuote = await getRandomQuote();
+    setQuote(randomQuote);
+    setQuoteColor(getRandomColor());
+  };
+
+  useEffect(() => {
+    async function fetchRandomQuote() {
+      const randomQuote = await getRandomQuote();
+      setQuote(randomQuote);
+    }
+    fetchRandomQuote();
+  }, []);
+
+  async function getQuotes() {
+    const url =
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const quotesData = await response.json();
+      return quotesData.quotes;
+    } catch (error) {
+      console.error("Error fetching quotes:", error);
+      return [];
+    }
+  }
+
+  async function getRandomQuote() {
+    const quotes = await getQuotes();
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  }
+
+  function getRandomColor() {
+    const red = Math.floor(Math.random() * 250);
+    const green = Math.floor(Math.random() * 250);
+    const blue = Math.floor(Math.random() * 250);
+    return `rgb(${red},${green},${blue})`;
+  }
+
+  const transition = "all 3s";
+
+  return (
+    <div
+      className="background"
+      style={{ backgroundColor: quoteColor, transition }}
+    >
+      <div id="quote-box">
+        <div className="quote-content">
+          <h2 id="text" style={{ color: quoteColor, transition }}>
+            <FaQuoteLeft
+              size="30"
+              style={{ marginRight: "10px", color: quoteColor, transition }}
+            />
+            {quote.quote}
+            <FaQuoteRight
+              size="30"
+              style={{ marginLeft: "10px", color: quoteColor, transition }}
+            />
+          </h2>
+          <h4 id="author" style={{ color: quoteColor, transition }}>
+            ~ {quote.author}
+          </h4>
+        </div>
+        <div className="buttons">
+          <a
+            href={
+              "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}"
+            }
+            id="tweet-quote"
+            style={{
+              backgroundColor: quoteColor,
+              marginRight: "10px",
+              transition,
+            }}
+          >
+            <FaTwitter color="white" />
+          </a>
+          <button
+            id="new-quote"
+            onClick={changeQuote}
+            style={{ backgroundColor: quoteColor, transition }}
+          >
+            Change Quote
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
